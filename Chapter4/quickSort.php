f557311f557b6120846be0ab17bc8ffd06b5deb9
Ibrahim I Musabah <i.musabah92@gmail.com>
2022-02-17 13:23:32 +0300

    quick sort function

--- Chapter4/quickSort.php
+++ Chapter4/quickSort.php
@@ -1,5 +1,23 @@
 <?php
 
+//Average case vs. worst case
+//The performance of quicksort heavily depends on the pivot you choose.
+//Suppose you always choose the first element as the pivot. And you
+//call quicksort with an array that is already sorted. Quicksort doesn’t
+//check to see whether the input array is already sorted. So it will still try
+//to sort it.
+/***+
+                Recap
+• D&C works by breaking a problem down into smaller and smaller
+pieces. If you’re using D&C on a list, the base case is probably an
+empty array or an array with one element.
+• If you’re implementing quicksort, choose a random element as the
+pivot. The average runtime of quicksort is O(n log n)!
+• The constant in Big O notation can matter sometimes. That’s why
+quicksort is faster than merge sort.
+• The constant almost never matters for simple search versus binary
+search, because O(log n) is so much faster than O(n) when your list
+gets big. */
 function quickSort(array $array)
 {
     if (count($array) < 2)
