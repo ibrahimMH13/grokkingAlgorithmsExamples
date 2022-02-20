37c58ce1488b43e9a7b5889d8f5274430f8aafb2
Ibrahim I Musabah <i.musabah92@gmail.com>
2022-02-20 11:24:41 +0300

    deque

---
+++ Chapter5/breadthSirstSearch.php
@@ -0,0 +1,17 @@
+<?php
+
+function search(){
+
+    $deque = new \Ds\Deque();
+    $deque->insert(0, "e");             // [e]
+    $deque->insert(1, "f");             // [e, f]
+    $deque->insert(2, "g");             // [e, f, g]
+    $deque->insert(0, "a", "b");        // [a, b, e, f, g]
+    $deque->insert(2, ...["c", "d"]);   // [a, b, c, d, e, f, g]
+
+    var_dump($deque);
+
+    die(var_dump($deque));
+}
+
+search();
